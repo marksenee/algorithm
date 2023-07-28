ce81f922c33629f61a0da9de7bf7064cdd5edde0
markseeun <mark233891@gmail.com>
2023-07-28 09:55:44 +0900

    console.log 삭제

--- Lv.0/홀짝에 따라 다른 값 반환하기.js
+++ Lv.0/홀짝에 따라 다른 값 반환하기.js
@@ -2,7 +2,6 @@ function solution (n) {
     let answer = 0;
     
     if (n%2 !==0) {
-        console.log(n)
         for (let i =1; i<=n; i++) {
             if (i%2 !==0) {
                 answer += i;
